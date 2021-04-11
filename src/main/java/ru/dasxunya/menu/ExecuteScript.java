b5ea11d29ddbd4a695de76b96604e37e9899fd2e
dasxunya <dasha.ipad.mail@gmail.com>
2021-04-11 15:00:32 +0300

    Добавление обработки некорректных случаев для execute_script

--- src/main/java/ru/dasxunya/menu/ExecuteScript.java
+++ src/main/java/ru/dasxunya/menu/ExecuteScript.java
@@ -4,6 +4,9 @@ package ru.dasxunya.menu;
 import ru.dasxunya.core.*;
 
 import java.io.*;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static ru.dasxunya.utils.Historian.*;
@@ -18,39 +21,41 @@ public class ExecuteScript {
      *
      * @param scanner the scanner
      */
-    public static void run(Scanner scanner)  {
-		AddHistory("execute_script");
+    public static void run(Scanner scanner) {
+        AddHistory("execute_script");
 
-		System.out.println("Считывание и исполнение скрипта: ");
+        System.out.println("Считывание и исполнение скрипта: ");
 
-		// https://www.java.com/ru/download/help/path.html
+        // https://www.java.com/ru/download/help/path.html
 
-		String filename = System.getenv(SYSTEM_VARIABLE);
+        String filename = System.getenv(SYSTEM_VARIABLE);
 
-		if (App.IS_EXECUTE_SCRIPT_STATE)
-		{
-			System.out.println("Нельзя выполнять скрипт внутри другого вызова скрипта!");
-		}
-		else {
-			FileReader fileReader = null;
-			try {
-				fileReader = new FileReader(filename);
-				Scanner newScanner = new Scanner(fileReader);
+        if (Files.exists(Paths.get(filename)) && !Files.isRegularFile(Paths.get(filename))) {
+            System.out.println("Нельзя передать специальный файл в качестве скрипта!");
+        }
+        else {
+            if (App.IS_EXECUTE_SCRIPT_STATE) {
+                System.out.println("Нельзя выполнять скрипт внутри другого вызова скрипта!");
+            } else {
+                FileReader fileReader = null;
+                try {
+                    fileReader = new FileReader(filename);
 
-				App.IS_EXECUTE_SCRIPT_STATE = true;
-				App.Menu(newScanner);
-				fileReader.close();
-				App.IS_EXECUTE_SCRIPT_STATE = false;
-			}
-			catch (FileNotFoundException | NullPointerException exception)
-			{
-				System.out.println("Файл не найден!");
-			} catch (IOException exception) {
-				System.out.println("Проблема с доступом к файлу!");
-			}
-		}
+                    Scanner newScanner = new Scanner(fileReader);
+
+                    App.IS_EXECUTE_SCRIPT_STATE = true;
+                    App.Menu(newScanner);
+                    fileReader.close();
+                    App.IS_EXECUTE_SCRIPT_STATE = false;
+                } catch (FileNotFoundException | NullPointerException exception) {
+                    System.out.println("Файл не найден!");
+                } catch (IOException exception) {
+                    System.out.println("Проблема с доступом к файлу!");
+                }
+
+            }
 
 
-
-	}
+        }
+    }
 }
